96cd0783d262a3658d676ab207aca42d483630c3
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-12-19 10:37:15 +0530

    Here the product links are used to access the html code of the product page

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get('https://www.nike.com/in/w/mens-running-shoes-37v7jznik1zy7ok').text
-
-soup = BeautifulSoup(html_text, 'lxml')
+mainSite = requests.get('https://www.nike.com/in/w/mens-running-shoes-37v7jznik1zy7ok').text
+soup = BeautifulSoup(mainSite, 'lxml')
 
 productCards = soup.find_all('div', class_ = 'product-card')
 
-productLinks = []
 for productCard in productCards:
-    productLink = productCard.find('a', class_ = 'product-card__link-overlay')
-    productLinks.append(productLink['href'])
+    productLink = productCard.find('a', class_ = 'product-card__link-overlay')['href']
 
-print(productLinks)
+    productSite = requests.get(productLink).text
+    soup = BeautifulSoup(productSite, 'lxml')
+    print(soup)
