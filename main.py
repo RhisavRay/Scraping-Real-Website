f6e6aa5633d8de86f7e66967ed1feaac11bbf3c8
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-12-19 10:21:13 +0530

    Extracted the value of the 'href' attribute of the anchor tag and appended them to a list. These are product links, which will be further used to access more info about the shoe

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ soup = BeautifulSoup(html_text, 'lxml')
 
 productCards = soup.find_all('div', class_ = 'product-card')
 
+productLinks = []
 for productCard in productCards:
     productLink = productCard.find('a', class_ = 'product-card__link-overlay')
+    productLinks.append(productLink['href'])
 
-    print(productLink)
+print(productLinks)
