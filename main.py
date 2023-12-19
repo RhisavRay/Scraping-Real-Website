d9317b520cba60f54085193048e2d1de920a4273
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-12-19 18:03:52 +0530

    Taken all data and formatted to store as a list of dictionaries, except the image URL. For that, I found a solution which uses a library called selenium. Will try that next

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 mainSite = requests.get('https://www.nike.com/in/w/mens-running-shoes-37v7jznik1zy7ok').text
 soup = BeautifulSoup(mainSite, 'lxml')
 
+productInfo = []
+
 productCards = soup.find_all('div', class_ = 'product-card')
 
 for productCard in productCards:
-    productLink = productCard.find('a', class_ = 'product-card__link-overlay')['href']
+    imgUrl = productCard.find('img', class_ = 'product-card__hero-image')['src']
+    # print(imgUrl)
 
-    productSite = requests.get(productLink).text
-    soup = BeautifulSoup(productSite, 'lxml')
-    print(soup)
+    name = productCard.find('div', class_ = 'product-card__title').text
+    # print(name)
+
+    price = productCard.find('div', class_ = 'product-price').text[6:]
+    # print(price)
+
+    info = {'name': name, 'price': price, 'imgUrl': imgUrl}
+
+    productInfo.append(info)
+
+print(productInfo)
